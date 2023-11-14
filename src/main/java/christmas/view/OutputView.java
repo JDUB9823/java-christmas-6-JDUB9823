d85db942a1d724fd72fe1aaeb4ec693e62e19064
JDUB9823 <jeong98231@gmail.com>
2023-11-14 13:02:52 +0900

    feat: 고객 안내 주의 사항 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -5,6 +5,9 @@ import java.text.DecimalFormat;
 public class OutputView {
     public static void printGreeting() {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        System.out.println("[주의사항] 총주문 금액 10,000원 이상부터 이벤트가 적용됩니다.");
+        System.out.println("[주의사항] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.");
+        System.out.println("[주의사항] 음료만 주문 시, 주문할 수 없습니다.");
     }
 
     public static void printEventPreviewHead() {
